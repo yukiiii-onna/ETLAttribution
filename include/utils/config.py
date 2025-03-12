2cc0781264641d04b07cb87c17d9776ddb09539f
yukiiii-onna <aparajita.jena1712@gmail.com>
2025-03-12 21:18:58 +0100

    Update config.py

--- include/utils/config.py
+++ include/utils/config.py
@@ -1,7 +1,10 @@
 # include/utils/config.py
 
-API_KEY = "9daf78cc-8f0b-4ebf-b813-b51d739219fe"
+API_KEY = "----------------------------"
 CONV_TYPE_ID = "type_1"
 API_URL = f"https://api.ihc-attribution.com/v1/compute_ihc?conv_type_id={CONV_TYPE_ID}"
 DB_PATH = "/usr/local/airflow/include/challenge.db"
 SQL_PATH = "/usr/local/airflow/include/create_tables.sql"
+
+# Define the export directory
+EXPORT_DIR = "/usr/local/airflow/include/reports"
